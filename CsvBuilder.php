4edcc33017edba0442005a0fb0a759ca2a5e0ac1
andyexeter <palmer.andy@gmail.com>
2017-01-05 11:14:11 +0000

    Update code formatting to follow PSR1/2 standards

--- CsvBuilder.php
+++ CsvBuilder.php
@@ -8,176 +8,193 @@ use Palmtree\ArgParser\ArgParser;
  * @package    Palmtree
  * @subpackage Csv
  */
-class CsvBuilder {
-	public static $defaultArgs = [
-		'filename'  => '',
-		'delimiter' => ',',
-		'enclosure' => '"',
-		'newLine'   => "\r\n",
-		'escape'    => [
-			'"' => '""',
-		],
-	];
+class CsvBuilder
+{
+    public static $defaultArgs = [
+        'filename'  => '',
+        'delimiter' => ',',
+        'enclosure' => '"',
+        'newLine'   => "\r\n",
+        'escape'    => [
+            '"' => '""',
+        ],
+    ];
 
-	protected $args = [];
+    protected $args = [];
 
-	/**
-	 * @var string
-	 */
-	protected $rows = '';
-	/**
-	 * @var string
-	 */
-	protected $headers = '';
+    /**
+     * @var string
+     */
+    protected $rows = '';
+    /**
+     * @var string
+     */
+    protected $headers = '';
 
-	protected $output;
+    protected $output;
 
-	protected $filename;
+    protected $filename;
 
-	public function __construct( $args = [] ) {
-		$this->args = $this->parseArgs( $args );
-	}
+    public function __construct($args = [])
+    {
+        $this->args = $this->parseArgs($args);
+    }
 
-	public function setData( $data, $headers = true ) {
-		if ( $headers ) {
-			$this->addHeaders( array_keys( reset( $data ) ) );
-		}
+    public function setData($data, $headers = true)
+    {
+        if ($headers) {
+            $this->addHeaders(array_keys(reset($data)));
+        }
 
-		$this->addRows( $data );
-	}
+        $this->addRows($data);
+    }
 
-	/**
-	 * @param array $headers
-	 */
-	public function addHeaders( $headers ) {
-		foreach ( $headers as $header ) {
-			$this->addHeader( $header );
-		}
-	}
+    /**
+     * @param array $headers
+     */
+    public function addHeaders($headers)
+    {
+        foreach ($headers as $header) {
+            $this->addHeader($header);
+        }
+    }
 
-	/**
-	 * @param array $rows
-	 */
-	public function addRows( $rows ) {
-		foreach ( $rows as $key => $row ) {
-			$this->addRow( $row );
-		}
-	}
+    /**
+     * @param array $rows
+     */
+    public function addRows($rows)
+    {
+        foreach ($rows as $key => $row) {
+            $this->addRow($row);
+        }
+    }
 
-	/**
-	 * @param string $row
-	 */
-	public function addRow( $row ) {
-		$this->rows .= $this->args['enclosure'] .
-		               implode( $this->args['enclosure'] . $this->args['delimiter'] . $this->args['enclosure'], $this->escape( $row ) ) .
-		               $this->args['enclosure'] .
-		               $this->args['newLine'];
-	}
+    /**
+     * @param string $row
+     */
+    public function addRow($row)
+    {
+        $this->rows .= $this->args['enclosure'] .
+                       implode($this->args['enclosure'] . $this->args['delimiter'] . $this->args['enclosure'],
+                           $this->escape($row)) .
+                       $this->args['enclosure'] .
+                       $this->args['newLine'];
+    }
 
-	/**
-	 * @param string $header
-	 */
-	public function addHeader( $header ) {
-		$this->headers .= $this->args['enclosure'] .
-		                  $this->escape( $header ) .
-		                  $this->args['enclosure'] .
-		                  $this->args['delimiter'];
-	}
+    /**
+     * @param string $header
+     */
+    public function addHeader($header)
+    {
+        $this->headers .= $this->args['enclosure'] .
+                          $this->escape($header) .
+                          $this->args['enclosure'] .
+                          $this->args['delimiter'];
+    }
 
-	public function getHeaders() {
-		if ( empty( $this->headers ) ) {
-			return '';
-		}
+    public function getHeaders()
+    {
+        if (empty($this->headers)) {
+            return '';
+        }
 
-		$headers = rtrim( $this->headers, $this->args['delimiter'] );
-		$headers .= $this->args['newLine'];
+        $headers = rtrim($this->headers, $this->args['delimiter']);
+        $headers .= $this->args['newLine'];
 
-		return $headers;
-	}
+        return $headers;
+    }
 
-	public function getRows() {
-		$rows = rtrim( $this->rows );
-		$rows .= $this->args['newLine'];
+    public function getRows()
+    {
+        $rows = rtrim($this->rows);
+        $rows .= $this->args['newLine'];
 
-		return $rows;
-	}
+        return $rows;
+    }
 
-	public function getOutput() {
-		if ( $this->output === null ) {
-			$this->output = $this->getHeaders() . $this->getRows();
-		}
+    public function getOutput()
+    {
+        if ($this->output === null) {
+            $this->output = $this->getHeaders() . $this->getRows();
+        }
 
-		return $this->output;
-	}
+        return $this->output;
+    }
 
-	/**
-	 * @throws \Exception
-	 */
-	public function download() {
-		if ( headers_sent() ) {
-			throw new \Exception( 'Unable to start file download. Response headers already sent.' );
-		}
+    /**
+     * @throws \Exception
+     */
+    public function download()
+    {
+        if (headers_sent()) {
+            throw new \Exception('Unable to start file download. Response headers already sent.');
+        }
 
-		header( 'Content-Type: application/octet-stream' );
-		header( 'Content-Description: File Transfer' );
-		header( 'Content-Transfer-Encoding: Binary' );
-		header( 'Content-Disposition: attachment; filename="' . $this->getFilename() . '"' );
+        header('Content-Type: application/octet-stream');
+        header('Content-Description: File Transfer');
+        header('Content-Transfer-Encoding: Binary');
+        header('Content-Disposition: attachment; filename="' . $this->getFilename() . '"');
 
-		header( 'Expires: 0' );
-		header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
-		header( 'Pragma: public' );
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
 
-		$output = $this->getOutput();
+        $output = $this->getOutput();
 
-		header( 'Content-Length: ' . mb_strlen( $output ) );
+        header('Content-Length: ' . mb_strlen($output));
 
-		print $output;
-		exit;
-	}
+        print $output;
+        exit;
+    }
 
-	public function write() {
-		return file_put_contents( $this->getFilename(), $this->getOutput() );
-	}
+    public function write()
+    {
+        return file_put_contents($this->getFilename(), $this->getOutput());
+    }
 
-	/**
-	 * @param mixed $data
-	 *
-	 * @return mixed
-	 */
-	protected function escape( $data ) {
-		if ( is_array( $data ) ) {
-			foreach ( $data as $key => $value ) {
-				$data[ $key ] = $this->escape( $value );
-			}
-		} else {
-			$data = strtr( $data, $this->args['escape'] );
-		}
+    /**
+     * @param mixed $data
+     *
+     * @return mixed
+     */
+    protected function escape($data)
+    {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = $this->escape($value);
+            }
+        } else {
+            $data = strtr($data, $this->args['escape']);
+        }
 
-		return $data;
-	}
+        return $data;
+    }
 
-	public function getFilename() {
-		$filename = ( empty( $this->filename ) ) ? time() . '.csv' : $this->filename;
+    public function getFilename()
+    {
+        $filename = (empty($this->filename)) ? time() . '.csv' : $this->filename;
 
-		return $filename;
-	}
+        return $filename;
+    }
 
-	/**
-	 * @param mixed $filename
-	 *
-	 * @return CsvBuilder
-	 */
-	public function setFilename( $filename ) {
-		$this->filename = $filename;
+    /**
+     * @param mixed $filename
+     *
+     * @return CsvBuilder
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	protected function parseArgs( $args = [] ) {
-		$parser = new ArgParser( $args, 'filename' );
-		$parser->parseSetters( $this );
+    protected function parseArgs($args = [])
+    {
+        $parser = new ArgParser($args, 'filename');
+        $parser->parseSetters($this);
 
-		return $parser->resolveOptions( self::$defaultArgs );
-	}
+        return $parser->resolveOptions(self::$defaultArgs);
+    }
 }
 
