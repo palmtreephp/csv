4190b51ecfa8a906de20a065fd07134fa5ac8298
andyexeter <palmer.andy@gmail.com>
2017-01-14 15:55:36 +0000

    Use PHP_EOL instead of \n

--- CsvParser.php
+++ CsvParser.php
@@ -174,7 +174,7 @@ class CsvParser implements \Iterator, \Countable
     protected function formatCell($cell, $toLower = false)
     {
         $cell = trim($cell);
-        $cell = str_replace($this->newLines, "\n", mb_convert_encoding($cell, 'UTF-8', mb_detect_encoding($cell)));
+        $cell = str_replace($this->newLines, PHP_EOL, mb_convert_encoding($cell, 'UTF-8', mb_detect_encoding($cell)));
 
         if ($toLower) {
             $cell = mb_strtolower($cell);
